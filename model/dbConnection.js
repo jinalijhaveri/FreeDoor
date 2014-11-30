30b9253b2497a7219aa77d404a719ac417118aed
jinalijhaveri <jinalijhaveri@gmail.com>
2014-11-30 01:59:49 -0800

    Added getOfferHistory Method

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -6,11 +6,9 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
-<<<<<<< HEAD
-	password : 'jerrymouse',
-=======
+
 	password : '1202',
->>>>>>> origin/master
+
 	port : '3306',
 	database : 'freedoor'
 });
@@ -35,7 +33,6 @@ exports.deleteProduct = function(callback,productId,categoryId){
 			
 }
 
-<<<<<<< HEAD
 exports.getCategories = function(callback){
 	var query = 'select * from category';
 	console.log('Inside: dbConnection.js');
@@ -51,20 +48,26 @@ exports.addCategories = function(callback,categoryName){
 	console.log('Inside: dbConnection.js');
 	connection.query(query, function(err, rows) {
 			//connection.release();
-=======
+	});
+}
+	
 exports.updateProduct = function(callback,productId,categoryId,productName,quantity,expectedOffer,description,expiryDate,isValid,newCategoryId){
 	var query = 'update product set productName="'+productName+'" ,quantity='+quantity+' ,expectedOffer="'+expectedOffer+'" ,productDesc="'+description+'"	,productExpiryDate="'+expiryDate+'" ,isValid='+isValid+' ,lastUpdated=now(),categoryId='+newCategoryId+' where productId='+productId+' and categoryId='+categoryId;
 	console.log("Update Product Query : "+query);
 	connection.query(query, function(err, rows) {
 			
->>>>>>> origin/master
 			callback(err, rows);
 	});
 			
 }
 
-<<<<<<< HEAD
-=======
+exports.getOfferHistory = function(callback,offerId){
+	var query = 'select * from offerhistory where offerId='+offerId;
+	console.log("Get Offer History : "+query);
+	connection.query(query, function(err, rows) {
+			
+			callback(err, rows);
+	});
+			
+}
 
-
->>>>>>> origin/master
